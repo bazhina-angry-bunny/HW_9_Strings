3fe089c1f74524c4fd076b443ad36c6f1a598d67
bazhina-angry-bunny <praga617@mail.ru>
2024-02-12 01:03:43 +0500

    Systematization of names for accounting reports.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+
+            String fullName = lastName + " " + firstName + " " + middleName;
+            System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+            String upperCase = fullName;
+            String result = upperCase.toUpperCase();
+            System.out.println("Ф. И. О. сотрудника для заполнения отчета — " + result);
+        }
+        {
+            String firstName = "Семён";
+            String middleName = "СЁмёнович";
+            String lastName = "ИВАНОВ-Митин";
+            String fullName = lastName + " " + firstName + " " + middleName;
+            fullName = fullName.replace("ё","е");
+            fullName = fullName.replace("Ё","Е");
+            String upperCase = fullName.toUpperCase();
+            System.out.println("Ф. И. О. сотрудника — " + upperCase);
+        }
+
     }
 }
