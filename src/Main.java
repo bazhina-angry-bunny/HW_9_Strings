72021d43ddb238a9ed7b456497639671ab8c5bb3
bazhina-angry-bunny <praga617@mail.ru>
2024-02-12 00:23:27 +0500

    Systematization of names in the accounting database.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+
+            String fullName = lastName + " " + firstName + " " + middleName;
+            System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+            String upperCase = fullName;
+            String result = upperCase.toUpperCase();
+            System.out.println("Ф. И. О. сотрудника — " + result);
+        }
+        {
+            String firstName = "Семён";
+            String middleName = "Семёнович";
+            String lastName = "Иванов";
+            String fullName = lastName + " " + firstName + " " + middleName;
+            String fullNameNew = fullName.replace("ё","е");
+            String upperCase = fullNameNew.toUpperCase();
+            System.out.println("Ф. И. О. сотрудника — " + upperCase);
+        }
+
     }
 }
